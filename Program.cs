052b9733e94144269ed838b88b47a0f0e3220b6b
N <121445609+debashir229@users.noreply.github.com>
2024-04-16 17:57:43 +0300

    HomeWork_3

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+class Student
+{
+    string ?_name;
+    string ?_surName;
+    List<List<int>> Grade = new List<List<int>>();
+    enum Lesson { Administrationer = 0, Designer = 1, Programmers = 2 };
+    int ?_age;
+    public string Name
+    {
+        get { return _name != null ? _name : "Не задано";  }
+        set { _name = value.ToUpper(); }
+    }
+    public string SurName
+    {
+        get { return _surName != null ? _surName : "Не задано"; }
+        set { _surName = value.ToUpper(); }
+    }
+    public int Age { get; set; }
+
+
+}
+
