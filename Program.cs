6ab992402eebc826f2d592a78afae6c9d92ae8be
N <121445609+debashir229@users.noreply.github.com>
2024-04-18 18:22:39 +0300

    Temporary

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Channels;
+
+abstract class Vehicle
+{
+    public virtual bool move();
+}
+
+class Car : Vehicle
+{
+    Random chance = new Random();
+    public override bool move()
+    {
+        int num = chance.Next(500, 1500);
+        return (num > 550 && num < 820 && num != 666);
+    }
+}
